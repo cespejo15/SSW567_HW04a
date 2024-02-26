2ba297465898c77cf5731d0f2ae35bb42090baf0
Cesar Espejo <cespejo@stevens.edu>
2024-02-25 22:02:32 -0500

    Updated GithubAPI.py

--- GithubAPI.py
+++ GithubAPI.py
@@ -7,14 +7,17 @@ def Repositories(username):
     if responseRepo.status_code == 200:
         userRepos = json.loads(responseRepo.text)
         if userRepos != []:
+            repoList = []
             for repo in userRepos:
                 responseCommits = requests.get(f"https://api.github.com/repos/{username}/{repo['name']}/commits")
                 if responseCommits.status_code == 200:
                     userCommits = json.loads(responseCommits.text)
                     count = len(userCommits)
-                    return f"Repo: {repo['name']} Number of commits: {count}"
+                    repoList.append(f"Repo: {repo['name']} Number of commits: {count}")
                 else:
-                    return f"Commit retrieval faliure for {repo['name']}"
+                    repoList.append(f"Commit retrieval faliure for {repo['name']}")
+            print(repoList)
+            return repoList
         else:
             return "User has no repositories."
     else:
