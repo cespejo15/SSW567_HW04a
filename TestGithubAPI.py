426922ec2c79df01ec5d63d055610aa08fa2d92a
Cesar Espejo <91222019+cespejo15@users.noreply.github.com>
2024-02-25 22:57:59 -0500

    Update TestGithubAPI.py

--- TestGithubAPI.py
+++ TestGithubAPI.py
@@ -12,19 +12,18 @@ class TestGithubAPI(unittest.TestCase):
 
     #Some random letters that shouldn't result in any repos
     def testNotExist(self):
-        self.assertEqual(Repositories("ALKHFJKJ"), "Repository retrieval faliure or Repository not found")
+        self.assertEqual(Repositories("ALKHFJKJ"), "Repository retrieval failure or Repository not found")
 
     def testing(self):
         expected_output = """Repo: csp Number of commits: 2
-        Repo: hellogitworld Number of commits: 30
-        Repo: helloworld Number of commits: 6
-        Repo: Mocks Number of commits: 10
-        Repo: Project1 Number of commits: 2
-        Repo: richkempinski.github.io Number of commits: 9
-        Repo: threads-of-life Number of commits: 1
-        Repo: try_nbdev Number of commits: 2
-        Repo: try_nbdev2 Number of commits: 5
-        """
+Repo: hellogitworld Number of commits: 30
+Repo: helloworld Number of commits: 6
+Repo: Mocks Number of commits: 10
+Repo: Project1 Number of commits: 2
+Repo: richkempinski.github.io Number of commits: 9
+Repo: threads-of-life Number of commits: 1
+Repo: try_nbdev Number of commits: 2
+Repo: try_nbdev2 Number of commits: 5"""
         self.assertEqual(Repositories("richkempinski"), expected_output)
 
 
