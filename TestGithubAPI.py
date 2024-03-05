075411b4843713ee50701987d527ee2883677cdc
Cesar Espejo <91222019+cespejo15@users.noreply.github.com>
2024-03-04 23:54:16 -0500

    Update TestGithubAPI.py

--- TestGithubAPI.py
+++ TestGithubAPI.py
@@ -1,48 +1,42 @@
-
 import unittest
-from unittest.mock import patch, MagicMock
+from unittest import mock
 
-from GithubAPI import Repositories      
-
+from GithubAPI import Repositories
 class TestGithubAPI(unittest.TestCase):
-#     #My other github account and a user with no repos
-#     def testNoRepo(self):
-#         self.assertEqual(Repositories("AlanAtrach"), "User has no repositories.")
-#         self.assertEqual(Repositories("ALEDSUS"), "User has no repositories.")
+    #My other github account and a user with no repos
+    @mock.patch('GithubAPI.requests')
+    def testNoRepo(self, mock_requests):
+        mock_response = mock.MagicMock()
+        mock_response.status_code = 200
+        mock_response.text = '[]'
+        mock_requests.get.return_value = mock_response
+        self.assertEqual(Repositories("AlanAtrach"), "User has no repositories.")
+        self.assertEqual(Repositories("ALEDSUS"), "User has no repositories.")
 
-#     #Some random letters that shouldn't result in any repos
-#     def testNotExist(self):
-#         self.assertEqual(Repositories("ALKHFJKJ"), "Repository retrieval failure or Repository not found")
-#         self.assertEqual(Repositories("1831hndjnwu"), "Repository retrieval failure or Repository not found")
+    #Some random letters that shouldn't result in any repos
+    @mock.patch('GithubAPI.requests')
+    def testNotExist(self, mock_requests):
+        mock_response = mock.MagicMock()
+        mock_response.status_code = 404
+        mock_requests.get.return_value = mock_response
+        self.assertEqual(Repositories("ALKHFJKJ"), "Repository retrieval failure or Repository not found")
+        self.assertEqual(Repositories("1831hndjnwu"), "Repository retrieval failure or Repository not found")
 
-#     def testing(self):
-#         expected_output = """Repo: csp Number of commits: 2
-# Repo: hellogitworld Number of commits: 30
-# Repo: helloworld Number of commits: 6
-# Repo: Mocks Number of commits: 10
-# Repo: Project1 Number of commits: 2
-# Repo: richkempinski.github.io Number of commits: 9
-# Repo: threads-of-life Number of commits: 1
-# Repo: try_nbdev Number of commits: 2
-# Repo: try_nbdev2 Number of commits: 5"""
-#         self.assertEqual(Repositories("richkempinski"), expected_output)
-#         # A random GitHub account with 1 repo
-#         self.assertEqual(Repositories("meczin171"),"Repo: meczin171 Number of commits: 3")
-    
-    @patch('GithubAPI.requests.get')
+    @mock.patch('GithubAPI.requests.get')
     def test_mock_request(self, mock_response):
-        #repo 
-        mock_repo = MagicMock()
+        #repo
+        mock_repo = mock.MagicMock()
         mock_repo.status_code = 200
         mock_repo.text = '[{"name": "anything"}]'
         #commit
-        mock_commit = MagicMock()
+        mock_commit = mock.MagicMock()
         mock_commit.status_code = 200
-        mock_commit.text = '[{"one": 1}, {"two":1}, {"three":1}]' 
+        mock_commit.text = '[{"one": 1}, {"two":1}, {"three":1}]'
 
         mock_response.side_effect = [mock_repo, mock_commit]
         result = Repositories("anyone")
         self.assertEqual(result, "Repo: anything Number of commits: 3")
+
 
 
 if __name__ == '__main__':
